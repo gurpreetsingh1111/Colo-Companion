24db024df9735d294a6306835ef2b14d0b7ba85a
Gurpreet2512Singh <84591513+gurpreetsingh1111@users.noreply.github.com>
2024-03-19 18:15:44 +0530

    Add files via upload

---
+++ ChatBot/app.py
@@ -0,0 +1,78 @@
+import streamlit as st
+from streamlit_chat import message
+from streamlit_extras.colored_header import colored_header
+from chatbot import MaverickChatbot
+
+st.set_page_config(page_title="Doctor CancerCare Companion Consultant", page_icon='💬')
+
+with st.sidebar:
+    st.image("https://upload.wikimedia.org/wikipedia/commons/4/4c/Hackathon-llm-2023.png")
+    st.markdown('# 🤖 Menu ')
+    st.markdown('''
+    
+    ### Purpose
+
+    This AI is a helpful and casual consultation doctor named Kamal, whom it's purpose is designed to improve the patient consultation experience when research and discuss around subject of colorectal cancer.
+
+    We believe this AI assisstant can help many patients worldwide.
+
+    🏀🏓🏈🎳⚾🏒🥊⛳🤿🏏🎾🎿🏐⛸️🤖
+
+    ###
+    ''')
+
+    st.markdown('💻 Source code on [Github] (https://github.com/oldbright22/Hack2023-Vectara-WithUI-Chatbot)')
+    st.markdown('👨‍💻 Made by FutureTech Mavericks (https://tinyurl.com/Discord-MaverickTeam) ')
+    
+    
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = ["👨‍💻 Hello!"]
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = ['']
+
+# Layout of input/response containers
+input_container = st.container()
+colored_header(label='', description='', color_name='blue-30')
+response_container = st.container()
+
+
+def get_text():
+    question = st.text_input("Your inquiry", "", key="input")
+    return question
+
+
+with input_container:
+    st.markdown("💬 Welcome my name is Kamal, I'm cancercare companion, how can I help you ?")
+    user_input = get_text()
+
+
+def generate_response(prompt):
+    chatbot = MaverickChatbot()
+    #db = chatbot.get_db_maverick()
+    response = chatbot.get_response_from_query(prompt)
+    print("after calling chatbot - response")
+    print(response)
+    return response
+
+## Conditional display of AI generated responses as a function of user provided prompts
+with response_container:
+    if user_input:
+        response = generate_response(user_input)
+        st.session_state.past.append(user_input)
+        st.session_state.generated.append(response)
+
+    if st.session_state['generated']:
+        for i in range(len(st.session_state['generated'])-1, -1, -1):
+            message(st.session_state["generated"][i], key=str(i), avatar_style="bottts-neutral", seed=90)
+            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user', avatar_style="avataaars-neutral", seed=10)
+
+
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+"""
+
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
---
+++ ChatBot/chatbot.py
@@ -0,0 +1,136 @@
+
+import os
+import requests
+
+from dotenv import find_dotenv, load_dotenv
+import streamlit as st
+
+
+class MaverickChatbot:
+    
+    def __init__(self):
+        load_dotenv(find_dotenv())
+
+    @st.cache_data(show_spinner=False)
+    def get_response_from_query(_self, query):
+        """
+        Function that generates a response to a customer question using LLama and Vectara DB
+        """
+        try:
+
+
+            madeitthisfar = "begin"
+
+            CUSTOMER_ID = st.secrets["CUSTOMER_ID"]
+            X_API_KEY = st.secrets["X_API_KEY"]
+            CUSTOMERID = st.secrets["CUSTOMERID"]
+            VTOKEN = st.secrets["VTOKEN"]
+            CORPUSID = st.secrets["CORPUSID"]    
+
+            madeitthisfar = f"(1){CUSTOMER_ID} (2) {X_API_KEY} (3) {CUSTOMERID} (4) {VTOKEN} (5) {CORPUSID} "
+
+            url = 'https://api.vectara.io/v1/query'
+
+
+            headers = {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'customer-id': CUSTOMER_ID,
+                'x-api-key': X_API_KEY
+            }
+
+
+            data = {
+                "query": [
+                    {
+                        "query": query,
+                        "start": 0,
+                        "numResults": 5,
+                        "corpusKey": [
+                            {
+                                "customerId": CUSTOMERID,
+                                "corpusId": CORPUSID,
+                                "semantics": "DEFAULT",
+                                "dim": [
+                                    {
+                                        "name": "string",
+                                        "weight": 0
+                                    }
+                                ],
+                                "metadataFilter": "part.lang = 'eng'",
+                                "lexicalInterpolationConfig": {
+                                    "lambda": 0
+                                }
+                            }
+                        ],
+                        "rerankingConfig": {
+                            "rerankerId": 272725717
+                        },
+                        "summary": [
+                            {
+                                "summarizerPromptName": "string",
+                                "maxSummarizedResults": 0,
+                                "responseLang": "string"
+                            }
+                        ]
+                    }
+                ]
+            }
+        
+        
+            madeitthisfar = "prior to api call - vectara"
+            #Perform API call - POST towards  VECTARA DB  
+            response = requests.post(url, headers=headers, json=data)
+
+            madeitthisfar = "after to api call - vectara {response.status_code}"
+            if response.status_code == 200:
+                response_json = response.json()
+
+                # Check if results are available
+                if "responseSet" in response_json and len(response_json["responseSet"]) > 0:
+                    ##############################################################################################################################
+                    # text = response narrow down from VectaraDB to a customer query that will be used later by Llama-2-70b-chat-hf as GUIDELINE
+                    ##############################################################################################################################
+                    for result in response_json["responseSet"][0]["response"]:
+                        text = result.get("text", "")
+                        madeitthisfar = "got a text narrow down from vectara"
+                        #print("Extracted Text:")
+                        #print(text)
+                else:
+                    return "1ST -- No results were found for the query."
+                    #print("No results were found for the query.")
+            else:
+                return f"2ND -- Request failed with status code: {response1.status_code}"
+                #print(f"Request failed with status code: {response.status_code}")
+
+
+            s = requests.Session()
+
+            madeitthisfar = "prior to LLM call"
+            #api_base = os.getenv("ANYSCALE_API_BASE")
+            api_base = "https://api.endpoints.anyscale.com/v1"
+            token = VTOKEN
+            url = f"{api_base}/chat/completions"
+            body = {
+                    "model": "meta-llama/Llama-2-70b-chat-hf",
+                    "messages": [{"role": "system", "content": "Your name is Kamal and you are a helpful and casual consultation doctor, you will extract and summarize factual information from {text} and give an accurate response(don't summarize links so write them as source), The is information from {text} is to prevent you from hallucination and guide you to provide factual information. Make sure you are conversational, use relevant emojis in some some cases. You understand how to handle compliment and greetings such as Hello, what's up, hey, Thank you etc. In a circumstance in which you don't have enough information just say 'I'm out of information, can say something different?' "}, 
+                                 {"role": "user", "content": query}],
+                                  "temperature": 0.7
+                   }
+
+            response = s.post(url, headers={"Authorization": f"Bearer {token}"}, json=body)
+
+            madeitthisfar = "after LLM call"
+            if response.status_code == 200:
+                response_json = response.json()
+                chatbot_response = response_json["choices"][0]["message"]["content"]
+                #print("Chatbot Response:")
+                madeitthisfar = "after LLM call" + chatbot_response
+                return chatbot_response
+            else:
+                return f"4th -- Request failed with status code: {response.status_code}"
+                #print(f"Request failed with status code: {response.status_code}")
+
+            
+        except:
+            return f"EXCEPTION - No response available at this time - how far in the code are we = {madeitthisfar}"
